79983c817acaad4c2aa19f01ca4d6bca5b948839
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-02 12:00:35 -0600

    intialzed arrays and made sure they're working with console.log

--- arraymethods.js
+++ arraymethods.js
@@ -6,18 +6,21 @@ let album = [];
 let artist = [];
 
 // INITIALIZE ARRAYS //
-song.push = ("So Far Away","Elementary","Reverie","Decades","Starlight Nights","Moonrays","Evade","salience","Dusk","Xtract");
-album.push = ("Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)");
-artist.push = ("Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016");
-
-// DOM SHIT //
+song.push("So Far Away","Elementary","Reverie","Decades","Starlight Nights","Moonrays","Evade","salience","Dusk","Xtract");
+album.push("Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)");
+artist.push("Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016");
+console.log(song);
+// GETTING DOM ELEMENTS //
 let selectDiv = document.getElementById("musicDiv");
 let selectSong = document.getElementById("songSec");
 let selectAlbum = document.getElementById("albumSec");
 let selectArtist = document.getElementById("artistSec");
 
-
-selectDiv.innerHTML = 
-`<section id="songSec"><label>song: </label></section>
-<section id="albumSec"><label>album: </label></section>
-<section id="artistSec"><label>artist: </label></section>`;
+// HTML WRITING //
+// selectDiv.innerHTML = 
+// `<section id="songSec"></section>
+// <section id="albumSec"></section>
+// <section id="artistSec"></section>`;
+// selectSong.innerHTML = `<label>song: </label>`;
+// selectAlbum.innerHTML = `<label>album: </label>`;
+// selectArtist.innerHTML = `<label>artist: </label>`;
