a135a2210bc732276406aa66bb39eb33ee3447ff
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-02 11:05:25 -0600

    added song, album, and artist arrays

--- arraymethods.js
+++ arraymethods.js
@@ -1,35 +1,8 @@
 console.log("javaScript linked");
-// Use JavaScript arrays and innerHTML to show the music you love.
-    // Create and modify arrays with join, push, reverse, sort, concat and unshift.
-    // Output lists to the DOM.
 
-// Create three separate arrays; one for songs, one for albums, and one for artists.
+// ARRAYS //
+let song = ["So Far Away","Elementary","Reverie","Decades","Starlight Nights","Moonrays","Evade","salience","Dusk","Xtract"];
 
-// The song array should be initialized with 10 songs.
+let album = ["Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)"];
 
-// Each subsequent array should hold the corresponding data (artist and album).
-    // For this exercise, each song is from a unique artist and album.
-
-// Create a display area on the page for each of the sections (songs,artists & albums).
-
-// Each display area should have a title/headline.
-
-// Using a form; Add 2 more songs.
-    // Each song should be added to the end of the song array.
-    // Each artist should be added to the beginning of the artist array.
-    // Each album should be added to the end of the album array.
-
-// For each output element arrange the corresponding data as follows:
-    // Output vertically descending alphabetically.
-    // Output vertically ascending alphabetically.
-    // Output vertically exactly as the data was added.
-    // Output vertically exactly as the data was added, inverted.
-    // Output horizontally descending alphabetically, separated by ,.
-    // Output horizontally ascending alphabetically, separated by >>.
-    // Output horizontally exactly as the data was added, separated by :.
-    // Output horizontally exactly as the data was added, inverted, separated by |.
-
-// Each output area should be separated by a headline tag.
-
-// Bonus
-    // Handle both upper and lowercase inputs. The way this is accomplished is up to you. Additional array methods may have to be employed to complete this bonus item.
+let artist = ["Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016"];
