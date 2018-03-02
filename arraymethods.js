323095e062395736244fbc6b9acfc38aa026af2d
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-02 11:43:27 -0600

    initialized arrays

--- arraymethods.js
+++ arraymethods.js
@@ -1,15 +1,19 @@
 console.log("javaScript linked");
 
-// ARRAYS //
-let song = ["So Far Away","Elementary","Reverie","Decades","Starlight Nights","Moonrays","Evade","salience","Dusk","Xtract"];
+// CREATE ARRAYS //
+let song = [];
+let album = [];
+let artist = [];
 
-let album = ["Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)"];
+// INITIALIZE ARRAYS //
+song.push = ("So Far Away","Elementary","Reverie","Decades","Starlight Nights","Moonrays","Evade","salience","Dusk","Xtract");
+album.push = ("Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)");
+artist.push = ("Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016");
 
-let artist = ["Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016"];
-
-// DOM SHIT
+// DOM SHIT //
 let selectDiv = document.getElementById("musicDiv");
 
-selectDiv.innerHTML = `<section id="songSec"><label>song: </label></section>
+selectDiv.innerHTML = 
+`<section id="songSec"><label>song: </label></section>
 <section id="albumSec"><label>album: </label></section>
 <section id="artistSec"><label>artist: </label></section>`;
