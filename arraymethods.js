3be2a45a2a711c9d3d050743cf92e6ab3c225009
Taylor Bailey <taylor.williams.bailey@gmail.com>
2018-03-02 11:36:36 -0600

    added sections with javascript

--- arraymethods.js
+++ arraymethods.js
@@ -6,3 +6,10 @@ let song = ["So Far Away","Elementary","Reverie","Decades","Starlight Nights","M
 let album = ["Visitors(2023)","Elementary(2029)","Reverie(199X)","Decades(2339)","Starlight Nights(2X9Z)","Moonrays(2011)","Descend(2146)","salience(5419)","Dusk(2001)","Xtract(3002)"];
 
 let artist = ["Lazerhawk","Zane Alexander","oDDling","Skjalg A. Skagen","Maya & Starforce","MoweLan","Emil Rottmayer","hello meteor","HOME","Audiotool Day 2016"];
+
+// DOM SHIT
+let selectDiv = document.getElementById("musicDiv");
+
+selectDiv.innerHTML = `<section id="songSec"><label>song: </label></section>
+<section id="albumSec"><label>album: </label></section>
+<section id="artistSec"><label>artist: </label></section>`;
